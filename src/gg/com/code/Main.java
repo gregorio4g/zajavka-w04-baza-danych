2833f139bd942ba266f83aa797e184567899a052
gregorio4g <gregorio4g@gmail.com>
2023-02-15 01:41:26 +0100

    Initial commit #2

---
+++ src/gg/com/code/Main.java
@@ -0,0 +1,39 @@
+package gg.com.code;
+
+public class Main {
+	public static void main(String[] args) {
+		// CRUD - create, read, update, delete
+		// DBMS - PostgreSQL, Oracle, MySQL, MS SQL Server, IBM Db2, MS Access, SQLite
+		// Database Server - Client
+		// Czym są dane?
+
+		// ID		| Imię     	| Nazwisko		| City
+		// 		1	| Karol			| Rogowski		| Wrocław
+		// 		2	| Karol			| Rogowski		|
+		// 		3 | Bartosz		| Kowalczyk		| Zakopane
+		// 		4	| Agnieszka	| Zajavkowa		| Gdynia
+
+		// NoSQL
+
+		// SQL - Structured Query Language
+		// JDBC - Java DataBase Connectivity
+		// JPA - Java Persistence API
+		// RDBMS - Relational DataBase Management System
+
+		// Data integrity
+		// - entity integrity
+		// - domain integrity
+		// - referential integrity
+		// - user-defined integrity
+
+		// Constraints
+		// - NOT NULL
+		// - UNIQUE
+		// - DEFAULT
+		// - CHECK (IS JSON)
+		// - PRIMARY KEY
+		// - FOREIGN KEY
+
+		System.out.println("Hello world!");
+	}
+}
